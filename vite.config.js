c8fad06dfe0dac4a0eb8298caaed6ce94bd2cdbb
sami-29 <annabisami01@gmail.com>
2023-08-11 13:58:11 +0100

    added vite config

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        game: resolve(__dirname, "game/index.html"),
+      },
+    },
+  },
+});
